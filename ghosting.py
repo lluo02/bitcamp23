42f3e31c9aea2606576882b696da7b13cf018fdb
Christopher Antar <cantar@terpmail.umd.edu>
2023-04-08 17:24:52 -0400

    added get request for front end

--- ghosting.py
+++ ghosting.py
@@ -3,14 +3,30 @@ from twilio.twiml.messaging_response import MessagingResponse
 import gptclient as gpt
 
 
+texts = []
+
 app = Flask(__name__)
+
+@app.route("/texts", methods=['GET'])
+def send_data():
+    if request.method == 'GET':
+        if texts[-1]:
+            return str(texts[-1])
+        else:
+            return ''
+
+
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
+
+
     """Send a dynamic reply to an incoming text message"""
  
     # Get the message the user sent our Twilio number
     body = request.values.get('Body', None)
+    # Get the number of the sender
+    sender_number = request.values.get('From', None)
     # Start our TwiML response
     resp = MessagingResponse()
 
@@ -21,6 +37,7 @@ def incoming_sms():
 
         resp.message(gpt_response)
 
+    texts.append((body, gpt_response))
     return str(resp)
 
 if __name__ == "__main__":
