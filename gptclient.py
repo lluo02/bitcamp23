a58646e53308577f8ddada128456f1da9a9f02a3
Luke Luo <lluo6658@gmail.com>
2023-04-09 01:06:25 -0400

    Changing the response model a bit

--- gptclient.py
+++ gptclient.py
@@ -2,26 +2,20 @@ import openai,os,sys
 from dotenv import load_dotenv
 
 load_dotenv()
-openai.api_key = os.getenv('GPT_API_KEY')
-
-openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-        {"role": "system", "content": "You are a very kind help desk agent."}
-        ])
-
+openai.api_key = os.getenv('API_KEY')
 
 def generate_response(message):
     prompt = f"User: {message}\nChatGPT:"
-    response = openai.Completion.create(
-      engine="text-davinci-003",
-      prompt = prompt,
+    response = openai.ChatCompletion.create(
+      model='gpt-3.5-turbo',
+      messages = [{"role": "system", "content": "You are a human being who does not want to engage or be bothered."},
+                  {"role": "user", "content": message}],
       max_tokens=1024,
       n=1,
       stop=None,
       temperature=0.7,
     )
-    message = response.choices[0].text.strip()
+    message = response.choices[0].message.content
     return message
 
 
@@ -29,27 +23,28 @@ def generate_response(message):
 
 def generate_friend_response(message):
     prompt = f"User: {message}\nChatGPT:"
-    response = openai.Completion.create(
-      engine="text-davinci-003",
-      prompt = prompt,
+    response = openai.ChatCompletion.create(
+      model='gpt-3.5-turbo',
+      messages = [{"role": "system", "content": "You are a college student being texted by a friend. Answer with a text message. Do not answer homework questions."},
+                  {"role": "user", "content": message}],
       max_tokens=1024,
       n=1,
       stop=None,
       temperature=0.7,
     )
-    message = response.choices[0].text.strip()
+    message = response.choices[0].message.content
     return message
 
 def generate_parent_response(message):
     prompt = f"User: {message}\nChatGPT:"
-    response = openai.Completion.create(
-      engine="text-davinci-003",
-      # Maybe ChatCompletion is different from Completion and that has a message parameter
-      prompt = prompt,
+    response = openai.ChatCompletion.create(
+      model='gpt-3.5-turbo',
+      messages = [{"role": "system", "content": "Answer with a text message, but do not say anything that might make the recipient worry, because they are family."},
+                  {"role": "user", "content": message}],
       max_tokens=1024,
       n=1,
       stop=None,
       temperature=0.7,
     )
-    message = response.choices[0].text.strip()
+    message = response.choices[0].message.content
     return message
